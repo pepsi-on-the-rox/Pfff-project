d5a5b0c02ae2f2169caa220fd35a5644f87649e0
Lukas <91724945+LukasMASchmitz@users.noreply.github.com>
2022-09-18 19:34:43 +0200

    Eerste commit

---
+++ Pfff/Pfff/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pfff.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
